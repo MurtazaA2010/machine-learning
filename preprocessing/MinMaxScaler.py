2e565e071bc511a9eb0ad67d4e75936db36525d1
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2026-01-17 19:39:49 +0600

    Update MinMaxScaler.py

--- preprocessing/MinMaxScaler.py
+++ preprocessing/MinMaxScaler.py
@@ -7,20 +7,18 @@ class MinMaxScaler:
         self.max_ = None
     def fit(self, X):
         X = np.array(X)
-        self.min_ = np.min(X)
-        self.max_ = np.max(X)
+        self.min_ = np.min(X, axis = 0)
+        self.max_ = np.max(X, axis = 0)
         return self
     def transform(self, X):
         if self.min_ is None or self.max_ is None:
             raise Exception("min max scaller not fitted yet!!")
         else:
+            X = np.array(X)
             if (self.max_ == self.min_):
-                return (X-self.min_) / ((self.max_ - self.min_) + 1e-9)  ##addint 1e-9 to make sure the denominator never equals to zero
+                return (X-self.min_) / ((self.max_ - self.min_) + 1e-9)  ##adding 1e-9 to make sure the denominator never equals to zero
             else:
                 return (X-self.min_) / ((self.max_ - self.min_))  
     def fit_transform(self, X):
         self.fit(X)
-        if (self.max_ == self.min_):
-            return (X-self.min_) / ((self.max_ - self.min_) + 1e-9)  
-        else:
-            return (X-self.min_) / ((self.max_ - self.min_))
+        return self.transform(X)
